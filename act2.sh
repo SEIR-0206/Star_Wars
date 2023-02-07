4f6bc96195c00d324597d234ab441214c7130ff6
JONPIRES <jonbpires@gmail.com>
2023-02-06 22:34:55 -0300

    partial act2

--- act2.sh
+++ act2.sh
@@ -3,9 +3,9 @@ echo "Help me, Obi-Wan…You’re my only hope." >> star_wars/rebellion/princess
 # 2. Create a file called obi_wan.txt in star_wars/rebellion
 touch star_wars/rebellion/obi_wan.txt
 # 3. Create a file in star_wars/rebellion called luke_skywalker.txt
-
+touch star_wars/rebellion/luke_skywalker.txt
 # 4. Create a directory in star_wars/rebellion called millenium_falcon
-
+mkdir star_wars/rebellion/millenium_falcon
 # 5. Inside the millenium_falcon, create two files: han_solo.txt and chewbacca.txt
 
 # 6. Move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon, respectively.
