7a99ba281e92303f06d81c30e67d2863a7e3ed77
JONPIRES <jonbpires@gmail.com>
2023-02-07 00:08:28 -0300

    act2 finished

--- act2.sh
+++ act2.sh
@@ -7,7 +7,8 @@ touch star_wars/rebellion/luke_skywalker.txt
 # 4. Create a directory in star_wars/rebellion called millenium_falcon
 mkdir star_wars/rebellion/millenium_falcon
 # 5. Inside the millenium_falcon, create two files: han_solo.txt and chewbacca.txt
-
+ touch star_wars/rebellion/millenium_falcon/han_solo.txt star_wars/rebellion/millenium_falcon/chewbacca.txt
 # 6. Move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon, respectively.
-
+mv star_wars/rebellion/luke_skywalker.txt star_wars/rebellion/obi_wan.txt star_wars/rebellion/princess_leia.txt star_wars/rebellion/millenium_falcon
 # 7. Move the millenium_falcon into the death_star.
+mv star_wars/rebellion/millenium_falcon star_wars/empire/death_star
