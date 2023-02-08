4fb2f967d024a50b8229720d1c9788d9e30556c1
Ali Colak <19acolak99@gmail.com>
2023-02-08 15:21:39 -0600

    Added my answers to act1-3.sh

--- act1.sh
+++ act1.sh
@@ -9,12 +9,56 @@ touch star_wars/empire/darth_vader.txt
 # 4. Use the force (or your echo) to add the text "...heavy breathing..." to the darth_vader.txt file (Don’t remember how to do this? Use the ‘other force’, known as Google!)
 echo "...heavy breathing..." >> star_wars/empire/darth_vader.txt 
 # 5. Inside the empire directory, create a file called emperor_palpatine.txt
-
+touch emperor_palpatine.txt
 # 6. Inside the empire directory, create a directory called death_star
+mkdir deathStar
+# 7. Move darth_vader.txt into the death_star. Assuming I'm in empire directory:
+mv darth_vader.txt deathStar
 
-# 7. Move darth_vader.txt into the death_star
+# Terminal Cheat Sheet
 
-#git add 
-#
-#git push
-#git remote -v 
+pwd         # print working directory
+ls          # list files in directory
+cd          # change directory
+~           # home directory
+..          # up one directory
+-           # previous working directory
+help        # get help
+-h          # get help
+--help      # get help
+man         # manual
+cat         # output the contents of a file
+mkdir       # create new directory
+open        # open a file with the associated program, a directory with Finder, or a URL with the default web browser
+ps          # list all running processes
+kill        # terminate existing process
+rmd         # permanently delete file
+rmdir       # remove directory
+
+## Working with Git
+
+### Quick Start
+git clone <url> 					# Clone directory
+git checkout -b <new-branch> 		# Create new local branch
+git push -u origin <new-branch> 	# Sync local branch with remote
+git checkout <branch> 				# Checkout branch
+git push origin <branch> 			# Push branch to remote
+
+git branch -d <branchname>   	# deletes local branch
+git push origin :<branchname>	# deletes remote branch
+
+git subtree push --prefix docs origin gh-pages  # push docs as subtree to gh-pages
+
+
+
+### Clone Directory
+git clone <url>
+
+
+
+### Create Project
+cd project/
+git init                    # initializes the repository
+git add .                   # add those 'unknown' files
+git commit                  # commit all changes, edit changelog entry
+git rm --cached <file>...   # ridiculously complicated command to undo, in case you forgot .gitignore
--- act2.sh
+++ act2.sh
@@ -1,13 +1,15 @@
 # 1. Inside the `star_wars/rebellion` directory, (IN ONE COMMAND) create a file called princess_leia.txt with the text "Help me, Obi-Wan…You’re my only hope."
-
+touch princess_leia.txt && echo "Help Me, Obi-Wan... Youre my only hope" >> princess_leia.txt
 # 2. Create a file called obi_wan.txt in star_wars/rebellion
-
+touch obi_wan.txt 
 # 3. Create a file in star_wars/rebellion called luke_skywalker.txt
-
+touch rebellion/luke_skywalker.txt
 # 4. Create a directory in star_wars/rebellion called millenium_falcon
-
+mkdir rebellion/millenium_falcon
 # 5. Inside the millenium_falcon, create two files: han_solo.txt and chewbacca.txt
-
+touch rebellion/millenium_falcon/han_solo.txt rebellion/millenium_falcon/chewbacca.txt
 # 6. Move luke_skywalker, obi_wan, and princess_leia into the millenium_falcon, respectively.
+mv obi_wan.txt luke_skywalker.txt princess_leia.txt millenium_falcon
+# 7. Move the millenium_falcon into the deathStar.
+mv rebellion/millenium_falcon empire/deathStar
 
-# 7. Move the millenium_falcon into the death_star.
